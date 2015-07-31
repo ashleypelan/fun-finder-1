1f2e6175a3b67492830e106c2f3c551c81ffd137
Ashley Pelan <ashley.pelan@gmail.com>
2015-07-31 14:32:35 -0600

    add unirest

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var unirest = require('unirest');
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
-
+// Events Page
 router.get('/events', function (req, res, next) {
   res.render('events');
+});
+
+router.post('/events', function (req, res, next) {
+  var category = 'outdoors_recreation'
+  var response = [];
+    unirest.get('http://api.eventful.com/json/events/search?app_key=' + process.env.EVENTFUL_KEY + '&keywords=' + category + '&location=Denver&date=Future', function (data) {
+      console.log(data.body[1]);
+      res.render('index', {info: JSON.stringify(data)})
+  });
 });
 
 
