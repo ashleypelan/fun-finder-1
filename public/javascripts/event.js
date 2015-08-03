732dd160a438f13c64d92a72a6a30da178877701
Micah Bell <micahmbell@gmail.com>
2015-08-03 15:38:37 -0600

    Dealt with duplication in randomizer function.

--- public/javascripts/event.js
+++ public/javascripts/event.js
@@ -2,17 +2,36 @@ var data = JSON.parse(document.getElementsByClassName('hidden')[0].innerHTML);
 var events = data;
 var title = document.getElementsByClassName('title')[0];
 var description = document.getElementsByClassName('description')[0];
+var exploreButton = document.getElementById('explore');
 var len = events.length;
+var duplicateCheck = [];
 
 var randomizer = function (events, len) {
   var randomInterest = Math.floor(Math.random() * len);
   var randomCat = Math.floor(Math.random() * len);
   var randomEvent = Math.floor(Math.random() * 9);
-  var specificEvent = events[randomInterest].categories[randomCat];
-  console.log(specificEvent.titles[randomEvent])
-  title.innerHTML = specificEvent.titles[randomEvent];
-  description.innerHTML = specificEvent.description[randomEvent];
-
+  var num = String(randomInterest) + String(randomCat) + String(randomEvent);
+  var trigger = 0;
+  for (var i = 0; i < duplicateCheck.length; i++) {
+    if(duplicateCheck[i] === num){
+      trigger ++;
+    }
+  }
+  if(trigger === 0) {
+    trigger = 0;
+    duplicateCheck.push(num);
+    var specificEvent = events[randomInterest].categories[randomCat];
+    title.innerHTML = specificEvent.titles[randomEvent];
+    description.innerHTML = specificEvent.description[randomEvent];
+  } else {
+    console.log(trigger);
+    trigger = 0;
+    randomizer(events, len);
+  }
 }
 
 randomizer(events, len);
+
+exploreButton.addEventListener('click', function () {
+  randomizer(events, len);
+})
